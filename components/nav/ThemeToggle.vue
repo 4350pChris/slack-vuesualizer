a4281788326b0bff406bc5dcb5f470ee236ec9ce
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-08-22 00:40:01 +0200

    looping sun :)

--- components/nav/ThemeToggle.vue
+++ components/nav/ThemeToggle.vue
@@ -2,13 +2,13 @@
   <button class="btn btn-ghost btn-circle" @click="dark = !dark">
     <span class="sr-only">Switch theme</span>
     <MoonIcon v-if="dark" class="h-8 w-8 text-sky-100" />
-    <SunIcon v-else class="h-8 w-8 text-amber-300" />
+    <SunIcon v-else class="h-8 w-8 text-amber-400" />
   </button>
 </template>
 
 <script lang="ts" setup>
 import MoonIcon from "~icons/line-md/moon";
-import SunIcon from "~icons/line-md/sunny-outline";
+import SunIcon from "~icons/line-md/sun-rising-twotone-loop";
 
 const colorMode = useColorMode();
 
