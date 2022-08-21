93cec22f1e44421389b0145aba50e87a2b491acf
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-08-21 12:18:55 +0200

    close side menu on click

--- components/nav/SideDrawer.vue
+++ components/nav/SideDrawer.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="drawer-side shadow">
-    <label for="drawer" class="drawer-overlay"></label>
-    <aside class="drawer-side bg-base-100 text-base-content w-80">
-      <a
-        href="/"
+    <label for="drawer" class="drawer-overlay" ref="toggle"></label>
+    <aside
+      class="drawer-side bg-base-100 text-base-content w-80"
+      @click="toggle.click()"
+    >
+      <NuxtLink
+        to="/"
         class="block text-xl p-2 my-2 mx-6 rounded text-gray-600 hover:text-base-content hover:bg-base-200 transition"
       >
-        Slack Vuesualizer
-      </a>
+        Workspace
+      </NuxtLink>
       <ul class="menu menu-compact py-2 px-4">
         <li class="menu-title">
           <span>Channels</span>
@@ -32,4 +35,6 @@ if (data.value.length === 0) {
 const sortedChannels = computed(() =>
   data.value.sort((a, b) => a.name.localeCompare(b.name))
 );
+
+const toggle = ref<HTMLElement>(null);
 </script>
