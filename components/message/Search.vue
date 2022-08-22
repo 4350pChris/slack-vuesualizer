1b3237023d3dcfa5e001e804b734778953ac5203
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-08-22 03:22:22 +0200

    add focus trap to search

--- components/message/Search.vue
+++ components/message/Search.vue
@@ -1,82 +1,84 @@
 <template>
   <div class="w-full max-w-xl">
-    <div class="w-full flex items-center gap-2 md:gap-4">
-      <input
-        v-if="visible"
-        type="text"
-        placeholder="Search messages"
-        class="input w-full font-mono"
-        v-model="query"
-        ref="input"
-      />
-      <button
-        v-else
-        class="btn btn-outline btn-block gap-4"
-        @click="visible = true"
-      >
-        <TextSearch class="w-6 h-6" />
-        <span class="font-mono">search messages</span>
-        <div class="hidden md:inline-block">
-          <kbd class="kbd text-base-content">Ctrl</kbd>
-          +
-          <kbd class="kbd text-base-content">K</kbd>
-        </div>
-      </button>
-    </div>
-    <Transition name="slide-y">
-      <div
-        v-if="visible"
-        class="px-2 pb-2 bg-base-100 shadow absolute top-16 h-[calc(100vh-4rem)] inset-x-0"
-      >
-        <div class="max-w-xl mx-auto h-full flex flex-col" ref="wrapper">
-          <div class="mb-2">
-            <div class="flex items-center">
-              <h3 class="font-medium text-lg flex-1">
-                Search Results for
-                <span class="font-bold">"{{ query }}"</span>
-                in
-                <span class="font-bold">{{
-                  allChannels ? "all channels" : route.params.channel
-                }}</span>
-              </h3>
-              <button
-                class="btn btn-circle btn-ghost flex-none"
-                @click="visible = false"
-                title="close"
-              >
-                <CloseIcon class="w-6 h-6" />
-              </button>
-            </div>
-            <div class="form-control">
-              <label class="max-w-max label cursor-pointer">
-                <input
-                  type="checkbox"
-                  v-model="allChannels"
-                  class="checkbox"
-                  :disabled="!route.params.channel"
-                />
-                <span class="font-mono label-text whitespace-nowrap ml-4">
-                  Search Everywhere
-                </span>
-              </label>
-            </div>
+    <component :is="visible ? FocusTrap : 'div'">
+      <div class="w-full flex items-center gap-2 md:gap-4">
+        <input
+          v-if="visible"
+          type="text"
+          placeholder="Search messages"
+          class="input w-full font-mono"
+          v-model="query"
+          ref="input"
+        />
+        <button
+          v-else
+          class="btn btn-outline btn-block gap-4"
+          @click="visible = true"
+        >
+          <TextSearch class="w-6 h-6" />
+          <span class="font-mono">search messages</span>
+          <div class="hidden md:inline-block">
+            <kbd class="kbd text-base-content">Ctrl</kbd>
+            +
+            <kbd class="kbd text-base-content">K</kbd>
           </div>
-          <Transition name="fade" mode="out-in">
-            <div
-              v-if="searching"
-              class="p-2 bg-base-100 w-full h-full flex justify-center mt-8"
-            >
-              <LoadingSpinner class="w-12 h-12" />
-            </div>
-            <MessageResults
-              class="min-h-0 overflow-auto"
-              v-else
-              :results="results"
-            />
-          </Transition>
-        </div>
+        </button>
       </div>
-    </Transition>
+      <Transition name="slide-y">
+        <div
+          v-if="visible"
+          class="px-2 pb-2 bg-base-100 shadow absolute top-16 h-[calc(100vh-4rem)] inset-x-0"
+        >
+          <div class="max-w-xl mx-auto h-full flex flex-col" ref="wrapper">
+            <div class="mb-2">
+              <div class="flex items-center">
+                <h3 class="font-medium text-lg flex-1">
+                  Search Results for
+                  <span class="font-bold">"{{ query }}"</span>
+                  in
+                  <span class="font-bold">{{
+                    allChannels ? "all channels" : route.params.channel
+                  }}</span>
+                </h3>
+                <button
+                  class="btn btn-circle btn-ghost flex-none"
+                  @click="visible = false"
+                  title="close"
+                >
+                  <CloseIcon class="w-6 h-6" />
+                </button>
+              </div>
+              <div class="form-control">
+                <label class="max-w-max label cursor-pointer">
+                  <input
+                    type="checkbox"
+                    v-model="allChannels"
+                    class="checkbox"
+                    :disabled="!route.params.channel"
+                  />
+                  <span class="font-mono label-text whitespace-nowrap ml-4">
+                    Search Everywhere
+                  </span>
+                </label>
+              </div>
+            </div>
+            <Transition name="fade" mode="out-in">
+              <div
+                v-if="searching"
+                class="p-2 bg-base-100 w-full h-full flex justify-center mt-8"
+              >
+                <LoadingSpinner class="w-12 h-12" />
+              </div>
+              <MessageResults
+                class="min-h-0 overflow-auto"
+                v-else
+                :results="results"
+              />
+            </Transition>
+          </div>
+        </div>
+      </Transition>
+    </component>
   </div>
 </template>
 
