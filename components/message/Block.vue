6002bcdcc9e975a58742faca6e328888c92a7691
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-08-24 20:26:21 +0200

    no more lazy imports for block

--- components/message/Block.vue
+++ components/message/Block.vue
@@ -1,30 +1,27 @@
 <template>
-  <LazyMessageBlocksRichText
+  <MessageBlocksRichText
     v-if="node.type === 'rich_text'"
     :key="node.block_id"
     :node="node"
   />
-  <LazyMessageBlocksRichTextSection
+  <MessageBlocksRichTextSection
     v-else-if="node.type === 'rich_text_section'"
     :node="node"
   />
-  <LazyMessageBlocksText v-else-if="node.type === 'text'" :node="node" />
-  <LazyMessageBlocksEmoji v-else-if="node.type === 'emoji'" :node="node" />
-  <LazyMessageBlocksLink v-else-if="node.type === 'link'" :node="node" />
-  <LazyMessageBlocksUser v-else-if="node.type === 'user'" :node="node" />
-  <LazyMessageBlocksBroadcast
-    v-else-if="node.type === 'broadcast'"
-    :node="node"
-  />
-  <LazyMessageBlocksRichTextQuote
+  <MessageBlocksText v-else-if="node.type === 'text'" :node="node" />
+  <MessageBlocksEmoji v-else-if="node.type === 'emoji'" :node="node" />
+  <MessageBlocksLink v-else-if="node.type === 'link'" :node="node" />
+  <MessageBlocksUser v-else-if="node.type === 'user'" :node="node" />
+  <MessageBlocksBroadcast v-else-if="node.type === 'broadcast'" :node="node" />
+  <MessageBlocksRichTextQuote
     v-else-if="node.type === 'rich_text_quote'"
     :node="node"
   />
-  <LazyMessageBlocksRichTextList
+  <MessageBlocksRichTextList
     v-else-if="node.type === 'rich_text_list'"
     :node="node"
   />
-  <LazyMessageBlocksChannel v-else-if="node.type === 'channel'" :node="node" />
+  <MessageBlocksChannel v-else-if="node.type === 'channel'" :node="node" />
   <p v-else class="text-warning">{{ node }}</p>
 </template>
 
