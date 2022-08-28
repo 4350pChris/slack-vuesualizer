4c93a03935ff076c1b7e9541c6b5b630c9868ebd
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-08-28 02:45:45 +0200

    add text search to user

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,15 +1,40 @@
 <template>
-  <section>
-    <div class="pt-4">
-      <ul class="list-none flex flex-col gap-4 md:gap-6">
-        <li v-for="user in users" :key="user.id">
-          <UserInfo :user="user" />
-        </li>
-      </ul>
-    </div>
+  <section class="flex flex-col gap-4 pt-4 h-full">
+    <h1 class="text-xl font-bold">Users</h1>
+    <input
+      type="text"
+      placeholder="Search users"
+      class="flex-none w-full max-w-lg input input-bordered"
+      v-model="query"
+    />
+    <TransitionGroup
+      name="list"
+      class="overflow-y-auto overflow-x-hidden list-none flex flex-col gap-6 md:gap-8"
+      tag="ul"
+    >
+      <li v-for="user in results" :key="user.id">
+        <UserInfo :user="user" />
+      </li>
+    </TransitionGroup>
   </section>
 </template>
 
 <script lang="ts" setup>
 const users = useUsers();
+
+const query = ref("");
+
+const results = useArrayFilter(users, ({ profile, real_name, name }) =>
+  [
+    name,
+    real_name,
+    profile.display_name,
+    profile.email,
+    profile.first_name,
+    profile.last_name,
+    profile.real_name,
+  ].some((s) =>
+    s?.toLocaleLowerCase().includes(query.value.toLocaleLowerCase())
+  )
+);
 </script>
