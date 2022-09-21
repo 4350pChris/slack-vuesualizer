1463f3b84496e71fee999f0a04d30385fe351482
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-09-21 16:31:17 +0000

    do not write in every processZip cycle, instead build array first and only write at the end to save trips to mongo

--- server/api/import/[name]/channel/[channel].post.ts
+++ server/api/import/[name]/channel/[channel].post.ts
@@ -1,24 +1,5 @@
 import { mongo } from "~/server/utils/mongo";
-import type { Db } from "mongodb";
-import { processZip, type Processor } from "~~/server/utils/zip";
-
-const makeFileProcessor =
-  (db: Db, channel: string): Processor =>
-  async (entry) => {
-    if (!(entry.type === "File" && entry.path.endsWith(".json"))) {
-      return;
-    }
-
-    const nameParts = entry.path.split("/");
-
-    if (channel === nameParts.at(-2)) {
-      const content = await entry.buffer();
-      const doc = JSON.parse(content.toString());
-      await db
-        .collection("messages")
-        .insertMany(doc.map((d) => ({ ...d, channel })));
-    }
-  };
+import { processZip } from "~~/server/utils/zip";
 
 export default defineEventHandler(async (event) => {
   const {
@@ -27,10 +8,25 @@ export default defineEventHandler(async (event) => {
   } = event.context;
 
   const db = await mongo(mongouuid);
+  const docs = [];
 
-  const processor = makeFileProcessor(db, channel);
+  await processZip(name, async (entry) => {
+    if (!(entry.type === "File" && entry.path.endsWith(".json"))) {
+      return;
+    }
 
-  await processZip(name, processor);
+    const nameParts = entry.path.split("/");
+
+    if (channel === nameParts.at(-2)) {
+      const content = await entry.buffer();
+      const data = JSON.parse(content.toString());
+      docs.push(...data);
+    }
+  });
+
+  await db
+    .collection("messages")
+    .insertMany(docs.map((d) => ({ ...d, channel })));
 
   event.res.statusCode = 201;
   return "ok";
