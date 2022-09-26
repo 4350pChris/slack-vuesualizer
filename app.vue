e255febc6dd9790a62d8296eb8c0fae5506e176c
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-09-26 12:27:13 +0700

    add vue-schema with basic configuration

--- app.vue
+++ app.vue
@@ -8,13 +8,30 @@
 </template>
 
 <script lang="ts" setup>
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+
 useHead({
   title: "Slack Vuesualizer",
   meta: [
+    {
+      name: "description",
+      content: t("description"),
+    },
     {
       name: "format-detection",
       content: "telephone=no",
     },
   ],
 });
+
+useSchemaOrg([
+  definePerson({
+    name: "Chris-Robin Ennen",
+    link: ["https://github.com/4350pChris"],
+  }),
+  defineWebSite({ name: "Slack Vuesualizer" }),
+  defineWebPage(),
+]);
 </script>
