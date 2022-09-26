7f78b8792302627d84cca33e6ae6b508a461d175
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-09-26 10:33:55 +0700

    more design stuff

--- components/upload/Success.vue
+++ components/upload/Success.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center flex flex-col gap-2">
+  <div class="text-center flex flex-col gap-4">
     <h2 class="text-lg font-bold">{{ $t("upload.success") }}</h2>
     <p>
       {{ $t("upload.done") }}
@@ -9,19 +9,17 @@
       :class="{ 'tooltip-open': copied }"
       :data-tip="copied ? $t('token.copied') : $t('token.copy')"
     >
-      <button class="btn btn-link gap-2" @click="copy()">
-        <ClipboardIcon class="w-4 h-4" />
+      <button class="btn btn-sm btn-link gap-2" @click="copy()">
         {{ link }}
       </button>
     </div>
-    <NuxtLink class="btn btn-primary btn-outline mt-4" to="/workspace">
+    <NuxtLink class="btn btn-primary btn-outline" to="/workspace">
       {{ $t("workspace.open") }}
     </NuxtLink>
   </div>
 </template>
 
 <script lang="ts" setup>
-import ClipboardIcon from "~icons/mdi/clipboard-edit-outline";
 const link = useShareLink();
 const { copy, copied } = useClipboard({ source: link });
 </script>
