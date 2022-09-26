9ebc750fb3de13b0d0167089de927a03434c54c5
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-09-26 10:12:07 +0700

    better looking success message after upload

--- components/upload/Success.vue
+++ components/upload/Success.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="text-center">
-    <p class="text-lg font-bold mb-2">{{ $t("upload.success") }}</p>
-    <i18n-t keypath="upload.done" tag="p">
-      <code class="block font-medium">{{ link }}</code>
-    </i18n-t>
+  <div class="text-center flex flex-col gap-2">
+    <h2 class="text-lg font-bold">{{ $t("upload.success") }}</h2>
+    <p>
+      {{ $t("upload.done") }}
+    </p>
+    <div
+      class="tooltip"
+      :class="{ 'tooltip-open': copied }"
+      :data-tip="copied ? $t('token.copied') : $t('token.copy')"
+    >
+      <button class="btn btn-link gap-2" @click="copy()">
+        <ClipboardIcon class="w-4 h-4" />
+        {{ link }}
+      </button>
+    </div>
     <NuxtLink class="btn btn-primary btn-outline mt-4" to="/workspace">
       {{ $t("workspace.open") }}
     </NuxtLink>
@@ -11,5 +21,7 @@
 </template>
 
 <script lang="ts" setup>
+import ClipboardIcon from "~icons/mdi/clipboard-edit-outline";
 const link = useShareLink();
+const { copy, copied } = useClipboard({ source: link });
 </script>
