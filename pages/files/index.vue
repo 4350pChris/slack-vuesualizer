397c2549aea97e6b7c39ac15ed943eef12f8de33
Chris-Robin Ennen <chris-robin@ennen.dev>
2022-09-30 08:36:27 +0700

    start work on filtering files

--- pages/files/index.vue
+++ pages/files/index.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="flex flex-col gap-4">
-    <div class="text-lg font-bold sticky top-0 bg-base-100 py-2">
-      <p>{{ $t("totalFiles", [files.length], files.length) }}</p>
-      <p>
-        {{
-          $t(
-            "hiddenFiles",
-            [files.length - notHidden.length],
-            files.length - notHidden.length
-          )
-        }}
-      </p>
+  <div class="flex flex-col gap-4 mb-2">
+    <div class="sticky top-0 bg-base-100 py-2">
+      <div class="text-lg font-bold mb-2">
+        <p>{{ $t("totalFiles", [files.length], files.length) }}</p>
+        <p>
+          {{
+            $t(
+              "hiddenFiles",
+              [files.length - notHidden.length],
+              files.length - notHidden.length
+            )
+          }}
+        </p>
+      </div>
+      <FilesFilter />
     </div>
     <ul class="list-none space-y-4">
       <li v-for="file in notHidden" :key="file.id">
--- server/api/users/index.get.ts
+++ server/api/users/index.get.ts
@@ -3,6 +3,10 @@ import type { User } from "~~/types/User";
 
 export default defineEventHandler(async (event) => {
   const db = await mongo(event.context.mongouuid);
-  const users = await db.collection<User>("users").find().toArray();
+  const users = await db
+    .collection<User>("users")
+    .find()
+    .sort({ is_bot: 1, "profile.display_name": 1, "profile.real_name": 1 })
+    .toArray();
   return users;
 });
