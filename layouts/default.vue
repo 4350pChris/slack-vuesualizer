ed74b47c1bfe4fb00646e8e1dd90ca96d508951c
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-06-05 16:48:01 +0200

    update modal to use proper dialog component
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,7 +27,7 @@ whenever(fetchedDms, d => (dms.value = d), { immediate: true })
 <template>
   <div>
     <NavHeader />
-    <div class="container mx-auto bg-base-100 drawer drawer-mobile">
+    <div class="container mx-auto bg-base-100 drawer lg:drawer-open">
       <input id="drawer" class="drawer-toggle" type="checkbox">
       <main
         class="h-[calc(100vh-4rem)] overflow-x-hidden drawer-content px-4 md:px-6 lg:px-8 mt-16 pb-2"
