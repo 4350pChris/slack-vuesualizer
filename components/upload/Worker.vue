29dbde5a2689434806f25efb82a7182850538b0d
Chris-Robin Ennen <chris-robin@ennen.dev>
2023-06-06 00:05:21 +0200

    move upload worker logic into composable
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- components/upload/Worker.vue
+++ components/upload/Worker.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { Sema } from 'async-sema'
 import type { Entry } from '@zip.js/zip.js'
 import ConfirmIcon from '~icons/line-md/confirm-circle'
 import AlertIcon from '~icons/line-md/alert-circle'
@@ -18,53 +17,7 @@ interface Emits {
 const props = defineProps<Props>()
 const emit = defineEmits<Emits>()
 
-const queue = ref(new Set<string>())
-const done = ref(new Set<string>())
-const errors = ref(new Set<string>())
-const retriable = ref(false)
-
-const { parseData } = useZip()
-
-const list = ref<HTMLElement>()
-
-const sema = new Sema(3)
-
-const uploadChannel = async (channel: string) => {
-  try {
-    await sema.acquire()
-    queue.value.add(channel)
-    const channelEntries = props.entries.filter(
-      e => !e.directory && e.filename.startsWith(`${channel}/`),
-    )
-
-    const data = await parseData(channelEntries)
-
-    // split into groups to prevent request from being too large for Vercel to handle
-    const groups = []
-    const size = 500
-
-    for (let i = 0; i < data.length; i += size)
-      groups.push(data.slice(i, i + size))
-
-    await Promise.all(
-      groups.map(group =>
-        $fetch(`/api/import/channel/${channel}`, {
-          method: 'POST',
-          body: { data: group },
-        }),
-      ),
-    )
-
-    done.value.add(channel)
-  }
-  catch (e) {
-    errors.value.add(channel)
-  }
-  finally {
-    queue.value.delete(channel)
-    sema.release()
-  }
-}
+const { queue, list, done, errors, retriable, doUpload } = useUpload()
 
 watchEffect(() => {
   const channel = props.channels.filter(c => queue.value.has(c)).at(-1)
@@ -82,66 +35,14 @@ watchEffect(() => {
   })
 })
 
-const uploadWorkspaceData = async () => {
-  try {
-    queue.value.add('vuesualizer-workspace')
-    const workspaceEntries = props.entries.filter(
-      e => !e.filename.includes('/') && !e.directory,
-    )
-    let data = await Promise.all(
-      workspaceEntries.map(async e => ({
-        name: e.filename.split('.json')[0],
-        data: await parseData([e]),
-      })),
-    )
+const handleUpload = async () => {
+  const success = await doUpload(props.channels, props.entries)
 
-    // remove channels that are not to be imported
-    data = data.map((d) => {
-      if (d.name !== 'channels')
-        return d
-
-      d.data = d.data.filter(c => props.channels.includes(c.name))
-      return d
-    })
-
-    // guard against empty datasets
-    data = data.filter(d => !(d.data.length === 1 && d.data[0] === ''))
-
-    await $fetch('/api/import/workspace', {
-      method: 'POST',
-      body: { data },
-    })
-
-    done.value.add('vuesualizer-workspace')
-  }
-  catch (e) {
-    errors.value.add('vuesualizer-workspace')
-    throw e
-  }
-  finally {
-    queue.value.delete('vuesualizer-workspace')
-  }
-}
-
-const doUpload = async () => {
-  retriable.value = false
-
-  if (!done.value.has('vuesualizer-workspace'))
-    await uploadWorkspaceData()
-
-  const channelsToUpload = props.channels.filter(
-    channel => !done.value.has(channel),
-  )
-
-  await Promise.all(channelsToUpload.map(uploadChannel))
-
-  if (done.value.size === props.channels.length + 1)
+  if (success)
     emit('done')
-  else
-    retriable.value = true
 }
 
-onMounted(doUpload)
+onMounted(handleUpload)
 </script>
 
 <template>
@@ -177,7 +78,7 @@ onMounted(doUpload)
         v-if="retriable"
         class="sticky bottom-0 -mb-4 bg-base-100 py-4 border-t"
       >
-        <button class="btn btn-outline btn-primary btn-block" @click="doUpload">
+        <button class="btn btn-outline btn-primary btn-block" @click="handleUpload">
           {{ $t("retry") }}
         </button>
       </div>
---
+++ composables/useUpload.ts
@@ -0,0 +1,121 @@
+import { Sema } from 'async-sema'
+import type { Entry } from '@zip.js/zip.js'
+
+export const useUpload = () => {
+  const queue = ref(new Set<string>())
+  const done = ref(new Set<string>())
+  const errors = ref(new Set<string>())
+  const retriable = ref(false)
+
+  const { parseData } = useZip()
+
+  const list = ref<HTMLElement>()
+
+  const sema = new Sema(3)
+
+  const uploadChannel = async (channel: string, entries: Entry[]) => {
+    try {
+      await sema.acquire()
+      queue.value.add(channel)
+      const channelEntries = entries.filter(
+        e => !e.directory && e.filename.startsWith(`${channel}/`),
+      )
+
+      const data = await parseData(channelEntries)
+
+      // split into groups to prevent request from being too large for Vercel to handle
+      const groups = []
+      const size = 500
+
+      for (let i = 0; i < data.length; i += size)
+        groups.push(data.slice(i, i + size))
+
+      await Promise.all(
+        groups.map(group =>
+          $fetch(`/api/import/channel/${channel}`, {
+            method: 'POST',
+            body: { data: group },
+          }),
+        ),
+      )
+
+      done.value.add(channel)
+    }
+    catch (e) {
+      errors.value.add(channel)
+    }
+    finally {
+      queue.value.delete(channel)
+      sema.release()
+    }
+  }
+
+  const uploadWorkspaceData = async (channels: string[], entries: Entry[]) => {
+    try {
+      queue.value.add('vuesualizer-workspace')
+      const workspaceEntries = entries.filter(
+        e => !e.filename.includes('/') && !e.directory,
+      )
+      let data = await Promise.all(
+        workspaceEntries.map(async e => ({
+          name: e.filename.split('.json')[0],
+          data: await parseData([e]),
+        })),
+      )
+
+      // remove channels that are not to be imported
+      data = data.map((d) => {
+        if (d.name !== 'channels')
+          return d
+
+        d.data = d.data.filter(c => channels.includes(c.name))
+        return d
+      })
+
+      // guard against empty datasets
+      data = data.filter(d => !(d.data.length === 1 && d.data[0] === ''))
+
+      await $fetch('/api/import/workspace', {
+        method: 'POST',
+        body: { data },
+      })
+
+      done.value.add('vuesualizer-workspace')
+    }
+    catch (e) {
+      errors.value.add('vuesualizer-workspace')
+      throw e
+    }
+    finally {
+      queue.value.delete('vuesualizer-workspace')
+    }
+  }
+
+  const doUpload = async (channels: string[], entries: Entry[]) => {
+    retriable.value = false
+
+    if (!done.value.has('vuesualizer-workspace'))
+      await uploadWorkspaceData(channels, entries)
+
+    const channelsToUpload = channels.filter(
+      channel => !done.value.has(channel),
+    )
+
+    await Promise.all(channelsToUpload.map(channel => uploadChannel(channel, entries)))
+
+    if (done.value.size === channels.length + 1)
+      return true
+
+    retriable.value = true
+    return false
+  }
+
+  return {
+    queue,
+    done,
+    errors,
+    retriable,
+    list,
+    doUpload,
+  }
+}
