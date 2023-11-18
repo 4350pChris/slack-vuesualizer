e0ff2f8f76c0d7b03f07d43fdba4d8cc525c5849
Chris-Robin Ennen <ce053@hdm-stuttgart.de>
2023-11-18 01:37:56 +0100

    fix matching of path to work with int routes
    Signed-off-by: Chris-Robin Ennen <ce053@hdm-stuttgart.de>

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,11 +1,12 @@
 export default defineNuxtRouteMiddleware((to, _) => {
   const urlToken = to.query.token
   const token = useCookie('mongouuid')
+  const localePath = useLocalePath()
 
   if (typeof urlToken === 'string')
     token.value = urlToken
 
-  if (token.value && to.path === '/')
+  if (token.value && to.path === localePath('/'))
     return navigateTo(`/workspace?token=${token.value}`)
 
   if (!token.value && to.meta.layout !== 'upload')
