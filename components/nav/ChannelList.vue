0892c858f0030717dc425717d431a050585e4321
Chris-Robin Ennen <chris-robin@ennen.dev>
2024-03-31 16:29:07 +0200

    refactor menu a bit
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

---
+++ components/nav/ChannelList.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup generic="T extends {id: string, name: string}">
+type Props = {
+  channels: T[]
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <details>
+    <summary class="capitalize">
+      <slot name="title" />
+    </summary>
+    <ul>
+      <li v-for="channel in channels" :key="channel.id" class="rounded-box">
+        <NuxtLinkLocale class="rounded-box" :to="`/channels/${channel.name}`">
+          <slot :channel="channel" name="channel" />
+        </NuxtLinkLocale>
+      </li>
+    </ul>
+  </details>
+</template>
