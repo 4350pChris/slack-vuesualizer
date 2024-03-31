ed6253ca18f57265935243f41409f8b12091db88
Chris-Robin Ennen <chris-robin@ennen.dev>
2024-03-31 20:17:23 +0200

    unify api for channels / dms / groups / mpims
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- composables/states.ts
+++ composables/states.ts
@@ -3,7 +3,3 @@ import type { Channel } from '~~/types/Channel'
 import type { User } from '~~/types/User'
 
 export const useUsers = () => useState<User[]>('users', () => [])
-export const useChannels = () => useState<Channel[]>('channels', () => [])
-export const useDms = () => useState<Dm[]>('dms', () => [])
-export const useGroups = () => useState<Channel[]>('groups', () => [])
-export const useMpims = () => useState<Channel[]>('mpims', () => [])
