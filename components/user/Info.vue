a963bb383b5f7afbefebaa3b78986749b589199a
Chris-Robin Ennen <chris-robin@ennen.dev>
2024-04-01 18:39:36 +0200

    virtualize user list
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- components/user/Info.vue
+++ components/user/Info.vue
@@ -13,10 +13,8 @@ defineProps<Props>()
 
 <template>
   <div class="flex justify-start gap-4 bg-base-100 text-base-content">
-    <UserAvatar
-      class="h-16 md:h-24 lg:h-44 w-16 md:w-24 lg:w-44 rounded-box"
-      :src="user.profile.image_192"
-    />
+    <UserAvatar :key="user.id" class="h-16 md:h-24 lg:h-44 w-16 md:w-24 lg:w-44 rounded-box"
+      :src="user.profile.image_192" />
     <div class="flex flex-col grow">
       <div class="justify-between mb-2 md:mb-4">
         <p class="font-medium">
@@ -32,16 +30,14 @@ defineProps<Props>()
       <div class="grid grid-cols-[1.5rem,minmax(0,max-content)] gap-2">
         <template v-if="user.profile.email">
           <EmailIcon class="w-5 h-5" />
-          <a
-            class="text-sm border-b-2 border-transparent hover:border-primary transition"
-            :href="`mailto:${user.profile.email}`"
-          >{{ user.profile.email }}</a>
+          <a class="text-sm border-b-2 border-transparent hover:border-primary transition"
+            :href="`mailto:${user.profile.email}`">{{ user.profile.email }}</a>
         </template>
         <template v-if="user.profile.phone">
           <PhoneIcon class="w-5 h-5" />
           <a class="font-mono text-sm" :href="`tel:${user.profile.phone}`">{{
-            user.profile.phone
-          }}</a>
+      user.profile.phone
+            }}</a>
         </template>
       </div>
     </div>
