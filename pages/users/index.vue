76d19b3e66ce7c9dbeee697671c5f832587f4b1c
Chris-Robin Ennen <chris-robin@ennen.dev>
2024-04-01 18:39:36 +0200

    virtualize user list
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup>
+import { breakpointsTailwind } from '@vueuse/core';
+import { RecycleScroller } from 'vue-virtual-scroller'
+
 const users = useUsers()
 
-const query = ref('')
+const query = useState('user-query', () => '')
 
 const results = useArrayFilter(users, ({ profile, real_name, name }) =>
   [
@@ -16,27 +19,27 @@ const results = useArrayFilter(users, ({ profile, real_name, name }) =>
     s?.toLocaleLowerCase().includes(query.value.toLocaleLowerCase()),
   ),
 )
+
+const { smaller } = useBreakpoints(breakpointsTailwind)
+
+const mobile = smaller('md')
+const tablet = smaller('lg')
+
+// h-16 md:h-24 lg:h-44 + py-4 in pixels
+const itemSize = computed(() => {
+  const padding = 16 * 2
+  if (mobile.value) return 64 + padding
+  if (tablet.value) return 96 + padding
+  return 176 + padding
+})
 </script>
 
 <template>
   <section class="flex flex-col gap-4 pt-4 h-full">
-    <input
-      v-model="query"
-      type="text"
-      :placeholder="$t('search.users')"
-      class="flex-none w-full max-w-lg input input-bordered"
-    >
-    <TransitionGroup
-      name="list"
-      class="overflow-y-auto overflow-x-hidden list-none flex flex-col gap-6 md:gap-8"
-      tag="ul"
-    >
-      <li v-for="user in results" :key="user.id">
-        <UserInfo :user="user" />
-      </li>
-      <li v-if="!results.length" key="empty">
-        {{ $t("noresults") }}
-      </li>
-    </TransitionGroup>
+    <input v-model="query" type="text" :placeholder="$t('search.users')"
+      class="flex-none w-full max-w-lg input input-bordered">
+    <RecycleScroller :prerender="10" class="h-full" :items="results" :item-size key-field="id" v-slot="{ item }">
+      <UserInfo class="py-4" :user="item" />
+    </RecycleScroller>
   </section>
 </template>
