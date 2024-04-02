e5d171ba23d7de2822de75edbd5077db2d3497c6
Chris-Robin Ennen <chris-robin@ennen.dev>
2024-04-02 04:33:28 +0200

    virtualize channels list
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

---
+++ components/channel/List.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { RecycleScroller } from 'vue-virtual-scroller';
+
+type Item = {
+  name: string,
+  label?: string
+}
+
+type Props = {
+  listItems: Item[]
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <RecycleScroller class="h-full" :items="listItems" :item-size="48" key-field="id" v-slot="{ item }">
+    <NuxtLinkLocale
+      class="h-12 p-4 rounded-box flex items-center channel-link hover:bg-base-200/50 dark:hover:bg-slate-200/10"
+      :to="`/channels/${item.name}`">
+      {{ item.label ?? item.name }}
+    </NuxtLinkLocale>
+  </RecycleScroller>
+</template>
