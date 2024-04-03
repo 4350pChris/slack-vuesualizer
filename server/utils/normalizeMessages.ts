a19a5b21c6dc710c7d96fd7204bc2c38fd5ed991
Chris-Robin Ennen <chris-robin@ennen.dev>
2024-04-03 02:55:20 +0200

    improve performance of normalizing messages - still bad but better
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- server/utils/normalizeMessages.ts
+++ server/utils/normalizeMessages.ts
@@ -2,33 +2,37 @@ import { assert } from '@vueuse/core'
 import type { ApiMessage, Message } from '~/types/Message'
 
 export default function (messages: ApiMessage[]): Message[] {
-  const cpy = [...messages]
+  const cpy: ApiMessage[] = []
+  const seen = new Set<string>()
 
   messages.forEach((m) => {
-    // replies isa empty or undefined - we can just leave it as is, replies will be cleaned up later
-    if (!m.replies)
+    if (seen.has(m.ts)) {
+      // this message is a reply, we already added it
       return
+    }
+
+    cpy.push(m)
+    seen.add(m.ts)
+
+    // replies is empty or undefined - we can just leave it as is, replies will be cleaned up later
+    if (!m.replies) {
+      return
+    }
 
     // for each reply, move it to the correct position in cpy
     m.replies.forEach((reply, i) => {
-      const index = cpy.findIndex(
+      const replyMessage = messages.find(
         m => m.ts === reply.ts && m.user === reply.user,
       )
       // should not happen...
-      assert(index > 0)
+      assert(reply !== undefined)
       // first remove from cpy
-      const spliced = cpy.splice(index, 1)
-      const withReplyProp = spliced.map(
-        s =>
-          ({
-            ...s,
-            reply: true,
-            last_reply: i === m.replies!.length - 1,
-          } as Message),
-      )
-      const parentMessageIndex = cpy.findIndex(({ _id }) => _id === m._id)
-      // then insert again at correct position, taking into account previously moved messages
-      cpy.splice(parentMessageIndex + i + 1, 0, ...withReplyProp)
+      cpy.push({
+        ...replyMessage,
+        reply: true,
+        last_reply: i === m.replies!.length - 1,
+      } as Message)
+      seen.add(reply.ts)
     })
   })
   return cpy
